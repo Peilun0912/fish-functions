7a606b95c0ee82c0bc6be42e96d9754da46da091
Pei-lun <Peilun0912@gmail.com>
2016-08-08 16:37:26 +0800

    Update add_gitignore

--- add_gitignore.fish
+++ add_gitignore.fish
@@ -1,7 +1,5 @@
 function add_gitignore
-  switch (echo $argv[1])
-    case Python
-      curl 'https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore' -o '.gitignore'
+  switch (echo $argv[1] | tr '[:upper:]' '[:lower:]')
     case python
       curl 'https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore' -o '.gitignore'
   end
